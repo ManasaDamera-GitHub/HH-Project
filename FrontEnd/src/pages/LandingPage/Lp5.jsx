d05276244875893fcfe4e82ee5d4253726aa1f11
DameraManasa-GitHub <dameramanasa25@gmail.com>
2025-05-26 12:50:11 +0530

    lp5

--- FrontEnd/src/pages/LandingPage/Lp5.jsx
+++ FrontEnd/src/pages/LandingPage/Lp5.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "../../styles/FifthPart.css"; // Import the CSS file
+import { useNavigate } from "react-router-dom";
 
 export const FifthPart = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/wall-panels");
+  };
   return (
-    <div className="fifth-part-container">
+    <div className="fifth-part-container" onClick={handleClick}>
       <div className="content-wrapper">
         <img src="./New/wall2.png" alt="wall image" className="wall-image" />
         <div className="text-content">
           <h1>Wall Panels</h1>
           <p>Level up your walls</p>
-          <button className="know-more-btn">Know more</button>
+          <button className="know-more-btn" onClick={handleClick}>
+            Know more
+          </button>
         </div>
       </div>
     </div>
